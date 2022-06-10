e072b74e20eebb2e0715c02bf3950dabdc06804c
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-06-09 22:15:18 -0400

    starting spring project

---
+++ src/main/java/com/example/springproject/Controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.example.springproject.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController  {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+}
+
